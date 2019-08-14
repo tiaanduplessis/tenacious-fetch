59edf5adcfaadbe8ec8091209e7845103d050ee3
Lotar Walace <lotar.walace@gmail.com>
2019-08-13 20:53:46 -0500

    cleanup todo

--- src/retrying-fetch.js
+++ src/retrying-fetch.js
@@ -2,42 +2,33 @@ import {linear, exponential} from './backoff'
 
 export default function retryingFetch (retries, url, config) {
   return new Promise((resolve, reject) => {
+    function retryAttempt (retriesLeft, url, config, value) {
+      if (retriesLeft > 0) {
+        retriesLeft--
+        const retryDelay = getRetryDelay(config, retriesLeft)
+
+        if (config.onRetry && typeof config.onRetry === 'function') {
+          config.onRetry({retriesLeft, retryDelay, response: value})
+        }
+
+        setTimeout(() => fetchAttempt(url, config, retriesLeft), retryDelay)
+      } else {
+        reject(value)
+      }
+    }
+
     function fetchAttempt (url, config, retriesLeft) {
-      let {retryStatus, fetcher} = config
+      const {retryStatus, fetcher} = config
       fetcher(url, config)
         .then(res => {
           if (retryStatus.includes(res.status)) {
-            // TODO: - Remove repetition
-            if (retriesLeft > 0) {
-              retriesLeft--
-              const retryDelay = getRetryDelay(config, retriesLeft)
-
-              if (config.onRetry && typeof config.onRetry === 'function') {
-                config.onRetry({retriesLeft, retryDelay, response: res})
-              }
-
-              setTimeout(() => fetchAttempt(url, config, retriesLeft), retryDelay)
-            } else {
-              reject(res)
-            }
+            retryAttempt(retriesLeft, url, config, res)
           } else {
             resolve(res)
           }
         })
         .catch(error => {
-          if (retriesLeft > 0) {
-            // TODO: - Remove repetition
-            retriesLeft--
-            const retryDelay = getRetryDelay(config, retriesLeft)
-
-            if (config.onRetry && typeof config.onRetry === 'function') {
-              config.onRetry({retriesLeft, retryDelay, response: error})
-            }
-
-            setTimeout(() => fetchAttempt(url, config, retriesLeft), retryDelay)
-          } else {
-            reject(error)
-          }
+          retryAttempt(retriesLeft, url, config, error)
         })
     }
 
